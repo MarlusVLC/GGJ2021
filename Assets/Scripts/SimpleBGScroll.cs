91390aa9665a5b9d97b4b69c71d0d811b6642cb6
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-01-28 20:12:25 -0300

    Shader iniciado. Nao finalizado

---
+++ Assets/Scripts/SimpleBGScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBGScroll : MonoBehaviour
+{
+    [SerializeField] float scrollSpeedX;
+    [SerializeField] private float scrollSpeedY;
+    
+    private Renderer _renderer;
+
+
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(scrollSpeedX, scrollSpeedY) * Time.deltaTime;
+        _renderer.material.mainTextureOffset += offset;
+
+    }
+}
