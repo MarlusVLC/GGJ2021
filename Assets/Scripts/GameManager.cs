7dca6bcd18bb2fa5264e7f91198deb19d3d60079
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-01-31 09:00:32 -0300

    Redemoinho completo. Precisa deixar a transicao mais suave

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+using Aux_Classes;
+
+public class GameManager : MonoBehaviour
+{
+    // [SerializeField] private float sloMoScale;
+    //
+    private Dictionary<GameObject, RespawnState> _respawnStates = new Dictionary<GameObject, RespawnState>();
+    // private bool _isTowerFocused;
+    //
+    // public delegate void EnterFocusMode();
+    // public event EnterFocusMode enterFocus;
+    //
+    // public delegate void ExitFocusMode();
+    // public event EnterFocusMode exitFocus;
+
+
+
+    private static GameManager INSTANCE;
+    public static GameManager getInstance
+    {
+        get { return INSTANCE; }
+    }
+
+    private void Awake()
+    {
+        //SINGLETON check
+        if (INSTANCE != null && INSTANCE != this)
+        {
+            Destroy(this.gameObject);
+        }
+        //SINGLETON instantiate
+        else
+        {
+            INSTANCE = this;
+        }
+        
+    }
+
+    private void Update()
+    {
+
+    }
+    
+
+
+    public void SetRespawnState(GameObject gameObject, Vector3 position, Vector3 velocity, float rotation)
+    {
+        RespawnState respawnState = new RespawnState(position, velocity, rotation);
+    
+    
+        if (_respawnStates.ContainsKey(gameObject))
+        {
+            _respawnStates[gameObject] = respawnState;
+        }
+        else
+        {
+            _respawnStates.Add(gameObject, respawnState);
+        }
+    }
+    
+    public RespawnState GetRespawnState(GameObject gameObject)
+    { 
+        return _respawnStates[gameObject];
+    }
+    //
+    //
+    // public bool IsTowerFocused
+    // {
+    //     get => _isTowerFocused;
+    //     set => _isTowerFocused = value;
+    // }
+    //
+    // public void EnterFocus()
+    // {
+    //     _isTowerFocused = true;
+    //     if (enterFocus != null)
+    //             enterFocus();
+    //     Time.timeScale = sloMoScale;
+    //     Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    //
+    // }
+    //
+    // public void ExitFocus()
+    // {
+    //     // _isTowerFocused = false;
+    //     if (exitFocus != null)
+    //         exitFocus();
+    //     Time.timeScale = 1;
+    //     Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    // }
+}
---
+++ Assets/Scripts/player/PlayerMisc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Aux_Classes;
+using UnityEngine;
+
+public class PlayerMisc : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        GameManager.getInstance.SetRespawnState(gameObject, transform.position, Vector3.zero, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("WhirlpoolsEye"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        RespawnState respawnState = GameManager.getInstance.GetRespawnState(this.gameObject);
+        transform.position = respawnState.Position;
+        _rb.rotation = respawnState.Rotation;
+        _rb.velocity = respawnState.Velocity;
+    }
+}
