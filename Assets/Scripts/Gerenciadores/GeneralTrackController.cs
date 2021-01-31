5f47aef500ad1123c75e95eb099587dfb0190761
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-01-30 22:13:52 -0300

    AudioManager e AudioController. Sfx de fundo e musica adicionados

---
+++ Assets/Scripts/Gerenciadores/GeneralTrackController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralTrackController : MonoBehaviour
+{
+    private AudioSource _bgTrackManager;
+    [SerializeField] AudioClip bgTrack0;
+
+
+    private void Awake()
+    {
+        _bgTrackManager = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        _bgTrackManager.clip = bgTrack0;
+        _bgTrackManager.Play();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
